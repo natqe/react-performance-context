46f5f6aab348eebad1dfc573fdb3fd2364b413df
natqe <33247529+natqe@users.noreply.github.com>
2021-08-02 07:39:35 +0300

    git init

---
+++ src/index.tsx
@@ -0,0 +1,51 @@
+import { createContext, FC, ProviderProps, useContext, useRef } from "react"
+
+type Hooks<Type> = {
+  [Property in keyof Type]: () => Type[Property]
+}
+
+export const createObjectContext = <T extends { [key: string]: any },>(defaultValue: T, displayName = `ObjectContext`) => {
+  const Ctx = createContext(defaultValue)
+  Ctx.displayName = displayName
+  const hooks = {} as Hooks<T>
+  const providers = []
+  for (const [key, value] of Object.entries(defaultValue)) {
+    const PropCtx = createContext(value)
+    PropCtx.displayName = `${key}Context`
+    hooks[key as keyof typeof hooks] = () => useContext(PropCtx)
+    const Provider = ({ children }) => {
+      const { [key]: value } = useContext(Ctx)
+      return <PropCtx.Provider {...{ value }}>{children}</PropCtx.Provider>
+    }
+    Provider.displayName = `${key}ContextWrapper`
+    providers.push(Provider)
+  }
+  const Provider: FC<ProviderProps<T>> = ({ children, value, ...props }) => {
+    return <Ctx.Provider {...{ value }} {...props}>
+      {providers.reduceRight((acc, Comp) => <Comp>{acc}</Comp>, children)}
+    </Ctx.Provider>
+  }
+  Provider.displayName = `${displayName}Wrapper`
+  return {
+    Provider,
+    use<T extends { [0]: keyof typeof hooks } & ReadonlyArray<keyof typeof hooks>>(...args: T) {
+      const initialArgs = useRef(args)
+      const result = {} as {
+        [key in typeof args[number]]: ReturnType<typeof hooks[key]>
+      }
+      for (const key of initialArgs.current) result[key] = hooks[key]()
+      return result
+    }
+  }
+}
+
+const methods = {
+  createObjectContext
+}
+
+const ReactPerformanceContext = {
+  ...methods,
+  ReactPerformanceContext: methods
+}
+
+export default ReactPerformanceContext
